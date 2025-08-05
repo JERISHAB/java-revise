a8958d3df4b0d09989eeffbffbaab5f9c7a54196
JERISHAB <jerishabmayan@gmail.com>
2025-08-05 16:14:13 +0530

    initial commit

---
+++ PrintName.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class PrintName {
+    public static void main(String[] args) {
+        // String name = "Jerishab";
+        // int age = 25;
+        // char initial = 'M';
+        // float height = 1.46f;
+        // boolean isStudent = true;
+
+        // System.out.println("Name: " + name);
+        // System.out.println("age: " + age);
+        // System.out.println("initial: " + initial);
+        // System.out.println("height: " + height);
+        // System.out.println("Student: " + isStudent);
+
+        // for (int i = 10; i > 0; i--) {
+        // System.out.println("The number is: " + i);
+        // }
+
+        // FUNCTIONS AKA METHODS IN JAVA
+
+        PrintName p = new PrintName();
+
+        p.sayHello();
+
+        // System.out.println("hello");
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the number");
+        // int age = sc.nextInt();
+        // System.out.println(age);
+        // sc.close();
+
+    }
+
+    void sayHello() {
+        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int age = sc.nextInt();
+        System.out.println(age);
+        sc.close();
+    }
+}
---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    String name = "Jerishab M";
+    int age = 22;
+    boolean isEnrolled;
+
+    Student(String n, int age) {
+        this.name = n;
+        this.age = age;
+    }
+}
