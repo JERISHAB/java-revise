0abc2bc35fed415d746343c60af92878bb30838b
JERISHAB <jerishabmayan@gmail.com>
2025-08-06 17:11:49 +0530

    feat: user class

---
+++ meeting-booking/User.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class User {
+
+    static ArrayList<MeetingRoom> meetingList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        User u = new User();
+        
+    }
+}
