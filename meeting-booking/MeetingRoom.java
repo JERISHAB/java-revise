6bb7b15d92c34a5838b84d55f3b84eeb842dd5de
JERISHAB <jerishabmayan@gmail.com>
2025-08-07 14:08:10 +0530

    feat: meeting room finished

--- meeting-booking/MeetingRoom.java
+++ meeting-booking/MeetingRoom.java
@@ -1,14 +1,16 @@
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class MeetingRoom {
 
     static int rooms = 0;
-    static ArrayList<String> bookingDates = new ArrayList<>();
+    ArrayList<LocalDate> bookingDates = new ArrayList<>();
     int roomId = 0;
-    boolean isActive = false;
-    boolean isBooked = false;
-    LocalDateTime bookingDate;
+    private boolean isActive = false;
+    private boolean isBooked = false;
+    LocalDate bookingDate;
 
     MeetingRoom() {
         rooms++;
@@ -16,32 +18,74 @@ public class MeetingRoom {
         this.isActive = true;
     }
 
-    void setActivity(boolean isActive) {
+    public void setActivity(boolean isActive) {
         this.isActive = isActive;
-        System.out.println("Activity set to: " + this.isActive);
+        System.out.println("ACTIVITY SET TO : " + this.isActive);
+        System.out.println();
+
     }
 
-    void setBooking(boolean bookFlag) {
-        LocalDateTime currentDate = LocalDateTime.now();
-        if (bookFlag) {
-            if (!(this.isBooked && (currentDate == bookingDate))) {
-                this.isBooked = true;
-                this.bookingDate = currentDate;
-                System.out.println("Booking successful");
+    public void setBooking(boolean bookFlag, LocalDate date) {
+        if (this.isActive) {
+            if (bookFlag) {
+                if (!(this.isBooked)) {
+                    this.isBooked = true;
+                    this.bookingDates.add(date);
+                    System.out.println("BOOKING SUCCESSFULL");
+                    System.out.println();
+
+                } else if (!(this.bookingDates.contains(date))) {
+                    this.bookingDates.add(date);
+                    System.out.println("BOOKING SUCCESSFULL");
+                    System.out.println();
+
+                } else {
+                    System.out.println("ROOM ALREADY BOOKED");
+                    System.out.println();
+
+                }
             } else {
-                System.out.println("Room already booked");
+                this.isBooked = false;
+                System.out.println("BOOKING CANCELLED");
+                System.out.println();
+
             }
         } else {
-            this.isBooked = false;
-            System.out.println("Booking cancelled");
+            System.out.println("BOOKING UNSUCCESSFUL : ROOM IS NOT ACTIVE");
+            System.out.println();
+
         }
+    }
+
+    public void bookingDetails() {
+        System.out.println(this.roomId);
+        System.out.println(this.bookingDates);
+        System.out.println("ACTIVITY : " + this.isActive);
+        System.out.println("BOOKED : " + this.isBooked);
+        System.out.println();
+
+    }
+    
+    public void displayMeetings() {
+        System.out.println(this.bookingDates);
+        System.out.println();
 
     }
 
-    void bookingDetails() {
-        System.out.println(this.roomId);
-        System.out.println(this.isActive ? "Room is active" : "Room is in-active");
-        System.out.println(this.isBooked ? "Room is Booked" : "Room is not-Booked");
+    public void displayVacantDates(YearMonth yearMonth){
+        int daysInMonth = yearMonth.lengthOfMonth();
+        ArrayList<LocalDate> daysList = new ArrayList<>();
+
+        for (int day = 1; day <= daysInMonth; day++) {
+            LocalDate date = yearMonth.atDay(day);
+            daysList.add(date);
+        }
+
+        daysList.removeAll(bookingDates);
+
+        System.out.println("DATES OF MONTH: " + daysList);
+        System.out.println();
+
     }
 
     public static void main(String[] args) {
@@ -53,10 +97,21 @@ public class MeetingRoom {
         // r1.setActivity(false);
 
         // set booking
-        // r1.setBooking(false);
+        String input = "07 09 2025";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
+        LocalDate date = LocalDate.parse(input, formatter);
+        r1.setBooking(true, date);
+
+
+        // display vacant days in month
+        // YearMonth yearMonth = YearMonth.of(2025, 9);
+        // r1.displayVacantDates(yearMonth);
+
+
 
         // room specific details
         r1.bookingDetails();
 
     }
 }
+ 
